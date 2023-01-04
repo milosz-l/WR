60f51fbdf53b6564397cc2cd328e44e9102633d2
Milosz Lopatto <milosz.lopatto@gmail.com>
2023-01-04 14:01:13 +0100

    refactoring

---
+++ colorfollower.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_D, MoveSteering, SpeedPercent, MediumMotor
+from ev3dev2.sensor import INPUT_1, INPUT_4
+from ev3dev2.sensor.lego import ColorSensor
+from time import sleep
+
+# define output names
+OUTPUT_A = 'outA'
+OUTPUT_B = 'outB'
+OUTPUT_D = 'outD'
+
+# define color sensors
+COLOR_SENSOR_R = ColorSensor(address=INPUT_1)
+COLOR_SENSOR_L = ColorSensor(address=INPUT_4)
+
+# define MoveSteering object
+STEERING_DRIVE = MoveSteering(OUTPUT_D, OUTPUT_A)
+
+# define arm motot
+ARM_MOTOR = MediumMotor(OUTPUT_B)
+
+# define speed
+GO_FORWARD_SPEED = 7
+TURN_SPEED = GO_FORWARD_SPEED
+
+# define light intensity threshold
+LIGHT_THRESHOLD = 18
+STEERING_SCALAR = 1
+SPEED_THRESHOLD = 25
+
+# define colors rgb lists
+RED_L = [130, 14, 17]
+RED_R = [210, 17, 35]
+BLUE_L = [16, 55, 90]
+BLUE_R = [27, 58, 180]
+GREEN_L = [12, 75, 32]
+GREEN_R = [22, 70, 63]
+WHITE_L = [100, 145, 110]
+WHITE_R = [180, 175, 255]
+BLACK_L = [10, 15, 15]
+BLACK_R = [15, 15, 30]
+MAX_EUCLIDEAN_DISTANCE = 510
+
+# define pick up and delivery color
+PACKAGE_COLOR = "Blue"
+DELIVERY_COLOR = "Green"
+
+# define the size of list with colors
+NUM_OF_COLORS_IN_LIST = 10
+# define how many colors in a row are needed to make a turn
+NUM_OF_COLORS_TO_TURN = 3
+# define how many colors in a row are needed to stop the car
+NUM_OF_COLORS_TO_STOP = 8
+
+
+def euclidean_distance(vx, vy):
+    '''
+    calculate euclidean distance between vectors
+    '''
+    return sum((y-x)**2 for x, y in zip(vx, vy)) ** 0.5
+
+
+class ColorFollower():
+
+    def __del__(self):
+        STEERING_DRIVE.off()
+
+    def get_colors(self):
+        '''
+        returns tuple of two strings: (left_color, right_color)
+        '''
+        color_r = COLOR_SENSOR_R.color
+        text_r = ColorSensor.COLORS[color_r]
+        color_l = COLOR_SENSOR_L.color
+        text_l = ColorSensor.COLORS[color_l]
+        return text_l, text_r
+
+    def rgb_to_intensity(self, color_sensor, sensor_is_left, color):
+        '''
+        color can be either Black, Blue, Green or Red
+        '''
+        rgb = color_sensor.rgb
+        if color == "Black":
+            if sensor_is_left:
+                rgb_to_compare = BLACK_L
+            else:
+                rgb_to_compare = BLACK_R
+        elif color == "Blue":
+            if sensor_is_left:
+                rgb_to_compare = BLUE_L
+            else:
+                rgb_to_compare = BLUE_R
+        elif color == "Green":
+            if sensor_is_left:
+                rgb_to_compare = GREEN_L
+            else:
+                rgb_to_compare = GREEN_R
+        elif color == "Red":
+            if sensor_is_left:
+                rgb_to_compare = RED_L
+            else:
+                rgb_to_compare = RED_R
+        else:
+            raise ValueError("invalid color passed")
+        intensity_rgb = euclidean_distance(rgb, rgb_to_compare) / 510 * 100
+        return intensity_rgb
+
+    def adjust_color_steering(self, color):
+        '''
+        calculates and returns steering based on rgb intensity and color given to follow
+        '''
+        calculated_intensity_l = self.rgb_to_intensity(COLOR_SENSOR_L, sensor_is_left=True, color=color)
+        calculated_intensity_r = self.rgb_to_intensity(COLOR_SENSOR_R, sensor_is_left=False, color=color)
+        # print("looking for", color, calculated_intensity_l, calculated_intensity_r)
+        # print("looking for", color)
+
+        # adjust intensity to be percentage
+        # light_sum = calculated_intensity_l + calculated_intensity_r
+
+        # light_difference = calculated_intensity_l - calculated_intensity_r
+        # light_difference = (calculated_intensity_l - calculated_intensity_r) / light_sum * 100
+        light_difference = (calculated_intensity_l - calculated_intensity_r)
+
+        # print(light_difference)
+        if light_difference > LIGHT_THRESHOLD:
+            return 100
+        if light_difference < -LIGHT_THRESHOLD:
+            return -100
+        if abs(light_difference) < 6:
+            return 0
+        # if light_sum < 15:
+        #     return 0
+        return light_difference*STEERING_SCALAR
+
+    def adjust_color_speed(self, steering):
+        '''
+        adjust speed based on turn angle
+        '''
+        if abs(steering) > SPEED_THRESHOLD:
+            return SpeedPercent(TURN_SPEED)
+        else:
+            return SpeedPercent(GO_FORWARD_SPEED)
+
+    def get_steering_and_speed_for_color_linefollowing(self, color):
+        '''
+        returns calculated steering and speed for given color
+        '''
+        color_steering = self.adjust_color_steering(color)
+        color_speed = self.adjust_color_speed(color_steering)
+        return color_steering, color_speed
+
+    def last_elements_are_specific_color(self, colors_list, n, specific_color):
+        '''
+        check whether last n elements in a list are given color
+        '''
+        return all([color == specific_color for color in colors_list[-n:]])
+
+    def append_colors_lists(self, l_colors, r_colors):
+        '''
+        append list of colors with new colors and remove older colors
+        '''
+        l_color, r_color = self.get_colors()
+        l_colors.append(l_color)
+        if len(l_colors) > NUM_OF_COLORS_IN_LIST:
+            l_colors.pop(0)
+        r_colors.append(r_color)
+        if len(r_colors) > NUM_OF_COLORS_IN_LIST:
+            r_colors.pop(0)
+        return l_colors, r_colors
+
+    def check_whether_to_turn(self, l_colors, r_colors):
+        '''
+        checks whether a turn should be made and returns 'left', 'right' or None
+        NOTE: at the moment it doesn't matter whether the function returns 'left' or 'right' - the robot just follows a line of a given color
+        '''
+        turn = None
+        if self.last_elements_are_specific_color(colors_list=l_colors, specific_color=PACKAGE_COLOR, n=NUM_OF_COLORS_TO_TURN):
+            if not self.last_elements_are_specific_color(colors_list=r_colors, specific_color=PACKAGE_COLOR, n=NUM_OF_COLORS_TO_TURN):
+                turn = 'left'
+        elif self.last_elements_are_specific_color(colors_list=r_colors, specific_color=PACKAGE_COLOR, n=NUM_OF_COLORS_TO_TURN):
+            turn = 'right'
+        return turn
+
+    def at_square(self, l_colors, r_colors, color):
+        '''
+        return whether robot is at square of given color
+        '''
+        return self.last_elements_are_specific_color(colors_list=l_colors, specific_color=color, n=NUM_OF_COLORS_TO_STOP) and self.last_elements_are_specific_color(colors_list=r_colors, specific_color=color, n=NUM_OF_COLORS_TO_STOP)
+
+    def deliver_package(self):
+        '''
+        function that runs whole delivery from start until the end
+        '''
+        l_colors = []
+        r_colors = []
+        while True:
+            # append colors lists
+            l_colors, r_colors = self.append_colors_lists(l_colors, r_colors)
+
+            # check whether a turn should be made
+            turn = self.check_whether_to_turn(l_colors, r_colors)
+            if turn == 'left' or turn == 'right':
+                print('STARTED TURNING')
+
+                # follow PACKAGE_COLOR line until both are the same color (then we are on color square)
+                l_colors = []
+                r_colors = []
+                while not self.at_square(l_colors, r_colors, PACKAGE_COLOR):
+                    # append colors lists
+                    l_colors, r_colors = self.append_colors_lists(l_colors, r_colors)
+                    steering, speed = self.get_steering_and_speed_for_color_linefollowing(PACKAGE_COLOR)
+                    STEERING_DRIVE.on(steering, speed)
+                print('AT PACKAGE COLOR SQUARE')
+
+                # stop at color
+                steering, speed = 0, 0
+                STEERING_DRIVE.on(steering, speed)
+
+                # pick up the package
+                ARM_MOTOR.on_for_degrees(speed=20, degrees=-50, brake=True, block=True)
+                sleep(1)
+
+                # move around 180 degrees
+                steering, speed = 100, 5
+                STEERING_DRIVE.on(steering, speed)
+                sleep(2)  # TODO: adjust time sleep
+
+                # put down the package # TODO: move this below green following
+                ARM_MOTOR.on_for_degrees(speed=20, degrees=50, brake=True, block=True)
+                sleep(1)
+
+                # follow DELIVERY_COLOR (green) until both sensors are the same color (then we are on color square)
+                print('FOLLOWING DELIVERY_COLOR')
+                l_colors = []
+                r_colors = []
+                while not self.at_square(l_colors, r_colors, DELIVERY_COLOR):
+                    # append colors lists
+                    l_colors, r_colors = self.append_colors_lists(l_colors, r_colors)
+                    steering, speed = self.get_steering_and_speed_for_color_linefollowing(DELIVERY_COLOR)
+                    STEERING_DRIVE.on(steering, speed)
+
+            else:   # if not making any turn just follow black line
+                steering, speed = self.get_steering_and_speed_for_color_linefollowing("Black")
+                STEERING_DRIVE.on(steering, speed)
+
+
+if __name__ == '__main__':
+    taczkowoz = ColorFollower()
+    taczkowoz.deliver_package()
